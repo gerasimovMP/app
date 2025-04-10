3bd688940612d0a39d276794f18b03fe67d30864
Max <gerasmaks@yandex.ru>
2025-04-10 15:17:38 +0300

    removed imports + comments

--- src/main/java/org/example/notificationservice/controller/OrderController.java
+++ src/main/java/org/example/notificationservice/controller/OrderController.java
@@ -4,7 +4,6 @@ import org.example.notificationservice.model.OrderDTO;
 import org.example.notificationservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
